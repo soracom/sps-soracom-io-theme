f2b629abebca17bd0f664fadfa0ab6e29c5f5bbf
takipone <takipone@gmail.com>
2020-10-13 16:39:08 +0900

    fix domain list again

--- strict_domains.js
+++ strict_domains.js
@@ -1,3 +1,3 @@
-if(!location.hostname.match(/soracom\.com$|soracom\.jp$|static\.getshifter\.io$|127\.0\.0\.1$/)){
+if(!location.hostname.match(/soracom\.com$|soracom\.jp$|static\.getshifter\.net$|127\.0\.0\.1$/)){
     location.href = "https://sps.soracom.io/";
 }
